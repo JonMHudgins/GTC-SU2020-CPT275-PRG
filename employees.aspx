1bd05d9bc249a32e1e95662a8e9f237aac88f78c
UnderEgag-alt <66091540+UnderEgag-alt@users.noreply.github.com>
2020-07-15 00:56:24 -0400

    New Employees Table Webpage Added
    Adds a new webpage directory to show the employees table and has nearly identical functionality as the items inventory table webpage.

---
+++ employees.aspx
@@ -0,0 +1,103 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="employees.aspx.cs" Inherits="employees" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <!-- div section for both search textboxes and submit buttons-->
+
+            <label for="employeeidtxt">Employee ID:</label><br />
+            <asp:Label ID="employeelab" runat="server" Text="E-"></asp:Label>
+            <asp:TextBox ID="employeeidtxt" runat="server"></asp:TextBox>
+            <asp:Button  OnClick="EmployeeIDSearch" runat="server" Text="Search Employee ID" />
+
+            <br />
+
+            <label for="">Employee Name:</label><br />
+            <asp:TextBox ID="employeenametxt" runat="server"></asp:TextBox>
+            <asp:Button OnClick="EmployeeNameSearch" runat="server" Text="Search Employee Name" />
+
+
+            <br />
+
+            <label for="departidtxt">Department ID:</label><br />
+            <asp:Label ID="departidlab" runat="server" Text="D-"></asp:Label>
+            <asp:TextBox ID="departidtxt" runat="server"></asp:TextBox>
+            <asp:Button  OnClick="DepartIDSearch" runat="server" Text="Search Department ID" />
+
+            <br />
+
+            <label for="departnametxt">Department Name:</label><br />
+            <asp:TextBox ID="departnametxt" runat="server"></asp:TextBox>
+            <asp:Button OnClick="DepartNameSearch" runat="server" Text="Search Department Name" />
+        </div>
+
+
+        <div>
+            <!-- div section for checkbuttons to hide or show columns -->
+
+            <asp:CheckBoxList ID="ColumnCheckBoxList" runat="server" AutoPostBack="True" OnSelectedIndexChanged="Check_Clicked" Width="262px">
+                <asp:ListItem Value="1">First Name</asp:ListItem>
+                <asp:ListItem Value="3">Admin</asp:ListItem>
+                <asp:ListItem Value="4">Phone</asp:ListItem>
+                <asp:ListItem Value="5">Email</asp:ListItem>
+                <asp:ListItem Value="6">Mail Info</asp:ListItem>
+                <asp:ListItem Value="10">Last Login</asp:ListItem>
+                <asp:ListItem Value="11">Department</asp:ListItem>
+            </asp:CheckBoxList>
+
+
+
+        </div>
+
+
+        <p></p>
+
+
+        <div>
+            <!-- div section for all radio buttons to filter for admins or not admins-->
+            <asp:RadioButton ID="RadBoth" runat="server" GroupName="adminstatus" Text="Both" AutoPostBack="true" OnCheckedChanged="RadBoth_CheckedChanged"/>
+            <asp:RadioButton ID="RadAdmin" runat="server" GroupName="adminstatus" Text="Only Admins" AutoPostBack="true" OnCheckedChanged="RadAdmin_CheckedChanged"/>
+            <asp:RadioButton ID="RadNonAdmin" runat="server" GroupName="adminstatus" Text="Only NonAdmins" AutoPostBack="true" OnCheckedChanged="RadNonAdmin_CheckedChanged"/>
+        </div>
+
+
+        <p></p>
+        <div>
+
+            
+            <!-- Table using asp GridView and connecting to database  This will also serve as the default style for now, Allows for sorting and paging.-->
+            <asp:GridView ID="EmployeeGridView" runat="server" AllowPaging="True" AllowSorting="True" AutoGenerateColumns="false" emptydatatext="No data available." OnSorting="ItemLookUp_Sorting" OnPageIndexChanging="OnPageIndexChanging" PageSize="2">
+                <Columns>
+                    
+                    <asp:TemplateField ItemStyle-Width="150px" HeaderText="Employee ID" SortExpression="EmployeeID">
+                        <ItemTemplate>
+                            <%# Eval("EmployeeID") %>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                    <asp:BoundField DataField="FirstName" HeaderText="First Name" ItemStyle-Width="150" sortexpression="FirstName"/>    
+                    <asp:BoundField DataField="LastName" HeaderText="Last Name" ItemStyle-Width="150" sortexpression="LastName"/>
+                    <asp:BoundField DataField="Admin" HeaderText="Admin" ItemStyle-Width="30" SortExpression="Admin" />
+                    <asp:BoundField DataField="Phone" HeaderText="Phone" ItemStyle-Width="150" sortexpression="Phone"/> 
+                    <asp:BoundField DataField="Email" HeaderText="Email" ItemStyle-Width="150" sortexpression="Email"/> 
+                    <asp:BoundField DataField="HomeAddress" HeaderText="Home Address" ItemStyle-Width="150" sortexpression="HomeAddress"/> 
+                    <asp:BoundField DataField="City" HeaderText="City" ItemStyle-Width="150" sortexpression="City"/> 
+                    <asp:BoundField DataField="ZIP" HeaderText="ZIP Code" ItemStyle-Width="30" SortExpression="ZIP" />
+                    <asp:BoundField DataField="State" HeaderText="State" ItemStyle-Width="150" SortExpression="State" />
+                    <asp:BoundField DataField="LastLogged" HeaderText="Last Login" ItemStyle-Width="150" SortExpression="LastLogged" />
+                    <asp:BoundField DataField="DepartmentID" HeaderText="Department ID" ItemStyle-Width="150" SortExpression="DepartmentID" />
+                    <asp:BoundField DataField="DepartmentName" HeaderText="Department Name" ItemStyle-Width="150" SortExpression="DepartmentName" />
+                
+                </Columns>
+            </asp:GridView>
+            <p></p>
+            
+        </div>
+    </form>
+</body>
+</html>
---
+++ employees.aspx.cs
@@ -0,0 +1,297 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+public partial class employees : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+
+        if (!Page.IsPostBack)
+        {
+
+
+            foreach (ListItem item in ColumnCheckBoxList.Items)
+            {
+                item.Selected = true;  //makes all of the checkboxlist items selected by default
+            }
+
+
+            //calls the show data method to fill table
+
+
+
+            this.BindGrid();
+
+        }
+
+
+    }
+
+    private string SortColumn //Private string keeps track of current preferred column for sorting with SKU as the default
+    {
+        get { return ViewState["SortColumn"] != null ? ViewState["SortColumn"].ToString() : "EmployeeID"; }
+        set { ViewState["SortColumn"] = value; }
+    }
+
+    private string SortDirection  //Private string keeps track of current preferred sorting direction with ascending as the default
+    {
+        get { return ViewState["SortDirection"] != null ? ViewState["SortDirection"].ToString() : "ASC"; }  //Condition on left views whether sortdirection is null if not it will return the found direction if it is null it will be ASC by default
+        set { ViewState["SortDirection"] = value; }
+    }
+
+    private string WhereClause  //Private string keeps track of last searched item to be used before sorting is null by default
+    {
+        get { return ViewState["WhereClause"] != null ? ViewState["WhereClause"].ToString() : null; }
+        set { ViewState["WhereClause"] = value; }
+
+    }
+
+    private string StatusFilter  //Private string to keep track of current selected status filter between requests
+    {
+        get { return ViewState["StatusFilter"] != null ? ViewState["StatusFilter"].ToString() : null; }
+        set { ViewState["StatusFilter"] = value; }
+    }
+
+    //Sort expression is the columnid being sent
+    private void BindGrid(string sortExpression = null, bool sort = false, bool where = false, string searchquery = null)  //Called to initially bind and display table on webpage with no sorting string by default || Search string is used to look for specific items with where clause statement
+    {
+        string constr = ConfigurationManager.ConnectionStrings["invDBConStr"].ConnectionString;
+
+        using (SqlConnection con = new SqlConnection(constr))  //Binds connection string to sql connection
+        {
+
+            string sqlquery = "SELECT Employees.*, DepartmentName FROM Employees JOIN Departments ON Departments.DepartmentID = Employees.DepartmentID ";  //The default query statement
+
+            if (where != false && searchquery != null) //Checks to see if a search is requested or not before sending statement and if it does a where clause is appended along with search string
+            {
+                sqlquery += "WHERE " + searchquery;
+                this.WhereClause = searchquery;
+                if (this.StatusFilter != null) //If statement checks to see if the status filter string is null and if not will append the filter to the query
+                {
+                    sqlquery += " AND " + this.StatusFilter;
+                }
+            }
+            else if (this.WhereClause != null)  //Second check to see if the sorting method or new page index was called and if so will append the sotred wherecluase statement
+            {
+                sqlquery += "WHERE " + this.WhereClause;
+                if (this.StatusFilter != null)
+                {
+                    sqlquery += " AND " + this.StatusFilter;
+                }
+            }
+            else if (this.StatusFilter != null) //checks to see if even in the event no other where clauses are being added that a status filter will still be appended
+            {
+                sqlquery += "WHERE " + this.StatusFilter;
+            }
+
+
+
+
+
+            using (SqlCommand cmd = new SqlCommand(sqlquery))  //The query string sent to database
+            {
+                using (SqlDataAdapter sda = new SqlDataAdapter())
+                {
+                    cmd.Connection = con;
+
+                    sda.SelectCommand = cmd;
+                    using (DataTable dt = new DataTable())
+                    {
+                        sda.Fill(dt);  //Fills the table with bound data
+                        if (sortExpression != null)  //Any future column sorts are sent here after initial load and appends the desired sort direction to the query string.
+                        {
+                            DataView dv = dt.AsDataView();
+
+                            if (sort) //checks to see if empty search was given in order to prevent sorting
+                            {
+                                this.SortDirection = this.SortDirection == "ASC" ? "DESC" : "ASC";  //swaps sorting direction if trying to use the same column to sort.
+                            }
+                            dv.Sort = sortExpression + " " + this.SortDirection;  //apends the column and sort direction to sort request.
+
+                            EmployeeGridView.DataSource = dv;
+                        }
+                        else   //The initial load of the page calls this if statement to give a default sort
+                        {
+                            EmployeeGridView.DataSource = dt;
+                        }
+                        EmployeeGridView.DataBind();
+                        this.SortColumn = sortExpression;  //Sends last used column to private string
+
+                    }
+                }
+            }
+        }
+    }
+
+    protected void ItemLookUp_Sorting(object sender, GridViewSortEventArgs e)  //Called when trying to sort columns on page.    ISSUE: Undoes the search query need to fix
+    {
+        this.BindGrid(e.SortExpression, true);  //Sends sort expression to refresh datatable
+    }
+
+    protected void OnPageIndexChanging(object sender, GridViewPageEventArgs e)  //Called when making use of paging on table when more than about 10 items by default
+    {
+        EmployeeGridView.PageIndex = e.NewPageIndex;
+        this.BindGrid(this.SortColumn);  //sends stored sorting column to reserve current sorting
+    }
+
+
+    /*  Needs sort direction img
+
+    // This is a helper method used to add a sort direction
+    // image to the header of the column being sorted.
+    protected void AddSortImage(int columnIndex, GridViewRow headerRow)
+    {
+
+        // Create the sorting image based on the sort direction.
+        Image sortImage = new Image();
+        if (ItemLookUpGridView.SortDirection == SortDirection.Ascending)
+        {
+            sortImage.ImageUrl = "~/Images/Ascending.jpg";
+            sortImage.AlternateText = "Ascending Order";
+        }
+        else
+        {
+            sortImage.ImageUrl = "~/Images/Descending.jpg";
+            sortImage.AlternateText = "Descending Order";
+        }
+
+        // Add the image to the appropriate header cell.
+        headerRow.Cells[columnIndex].Controls.Add(sortImage);
+
+    }
+    */
+
+
+
+
+    //Method called when searching for given Employee name
+    protected void EmployeeNameSearch(object sender, EventArgs e)
+    {
+        if (employeenametxt.Text != "")  //If condition on the case that the textbox being based on isnt empty
+        {
+            this.BindGrid(this.SortColumn, false, true, " CONCAT(FirstName, ' ', LastName) LIKE '%" + employeenametxt.Text + "%'");
+        }
+        else  //If the textbox is empty and the submit button is pressed it just refreshes the table. also sends true statement in order to prevent sorting
+        {
+            this.WhereClause = null;
+            this.RefreshTable("where");
+
+
+        }
+    }
+
+    //Method called when searching for ID of an Employee
+    protected void EmployeeIDSearch(object sender, EventArgs e)
+    {
+        if (employeeidtxt.Text != "")  //If condition on the case that the textbox being based on isnt empty
+        {
+            this.BindGrid(this.SortColumn, false, true, "EmployeeID= 'E-" + employeeidtxt.Text + "'");  //Automatically will have the characters E- for convience
+        }
+        else  //If the textbox is empty and the submit button is pressed it just refreshes the table. also sends true statement in order to prevent sorting.
+        {
+            this.WhereClause = null;
+            this.RefreshTable("where");
+
+        }
+    }
+
+    //Method called when searching for given department name
+    protected void DepartNameSearch(object sender, EventArgs e)
+    {
+        if (departnametxt.Text != "")  //If condition on the case that the textbox being based on isnt empty
+        {
+            this.BindGrid(this.SortColumn, false, true, " DepartmentName LIKE '%" + departnametxt.Text + "%'");
+        }
+        else  //If the textbox is empty and the submit button is pressed it just refreshes the table. also sends true statement in order to prevent sorting
+        {
+            this.RefreshTable("where");
+
+
+        }
+    }
+
+    //Method called when searching for id of a department
+    protected void DepartIDSearch(object sender, EventArgs e)
+    {
+        if (departidtxt.Text != "")  //If condition on the case that the textbox being based on isnt empty
+        {
+            this.BindGrid(this.SortColumn, false, true, "DepartmentID= 'D-" + departidtxt.Text + "'");  //Automatically will have the characters D- for convience
+        }
+        else  //If the textbox is empty and the submit button is pressed it just refreshes the table. also sends true statement in order to prevent sorting.
+        {
+            this.RefreshTable("where");
+
+        }
+    }
+
+
+
+
+
+
+
+
+    protected void Check_Clicked(object sender, EventArgs e)  //Method called when any member of the checkboxlist is updated and will refresh the table to see if any columns need to be hidden or shown.
+    {
+        foreach (ListItem item in ColumnCheckBoxList.Items)
+        {
+            
+            if(Int32.Parse(item.Value) == 6)  //If state to check if the Mail info group was selected and if so will either hide or show all mail related columns
+            {
+                for(int i = 6; i < 10; i++)  
+                {
+                    EmployeeGridView.Columns[i].Visible = item.Selected;
+                }
+            }
+            else if(Int32.Parse(item.Value) == 11)  //If statement checks if the department info ground was selected and will either hide or show all department related columns
+            {
+                EmployeeGridView.Columns[11].Visible = item.Selected;
+                EmployeeGridView.Columns[12].Visible = item.Selected;
+            }
+            else  //The default checker in the event a special group was not chosen and will hide or show column based on the checklist
+            {
+                EmployeeGridView.Columns[Int32.Parse(item.Value)].Visible = item.Selected;
+            }
+            
+        }
+    }
+
+    protected void RadBoth_CheckedChanged(object sender, EventArgs e) //Will refresh the table and set the admin status filter to null
+    {
+        this.StatusFilter = null;
+        this.RefreshTable();
+    }
+
+    protected void RadAdmin_CheckedChanged(object sender, EventArgs e) //Will refresh the table and set the admin status filter to only admins
+    {
+        this.StatusFilter = "Admin = 1";
+        this.RefreshTable();
+    }
+
+    protected void RadNonAdmin_CheckedChanged(object sender, EventArgs e) //Will refresh the table and set the admin status filter to only nonadmins
+    {
+        this.StatusFilter = "Admin = 0";
+        this.RefreshTable();
+    }
+
+
+    protected void RefreshTable(string element = null)  //Method called when refreshing a table without needing to completely remake a statement
+    {
+
+        if (element == "where")  //When refreshing a where clause it will also make the where clause null
+        {
+            this.WhereClause = null;
+        }
+
+
+        this.BindGrid(this.SortColumn, false);
+    }
+}
